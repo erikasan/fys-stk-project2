5cf3afcce993d552c3666d48a21f1e568de88044
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-11-15 14:50:00 +0100

    Fixed indentation error

--- programs/neural_network.py
+++ programs/neural_network.py
@@ -50,83 +50,83 @@ class NeuralNetwork:
         self.cost_function   = cost_function
         self.cost_derivative = cost_derivative
 
-        def feedforward(self, a):
-            """Return the output of the neural network.
+    def feedforward(self, a):
+        """Return the output of the neural network.
 
-            Parameters:
+        Parameters:
 
-                a : ndarray
-                    Input column vector. Must have shape (m, 1) where
-                    m is the number of nodes in the input layer. Alternatively
-                    several inputs can be fed forward simultaneously by providing
-                    an ndarray of shape (m, n)
+            a : ndarray
+                Input column vector. Must have shape (m, 1) where
+                m is the number of nodes in the input layer. Alternatively
+                several inputs can be fed forward simultaneously by providing
+                an ndarray of shape (m, n)
 
-            Returns:
+        Returns:
 
-                out : ndarray
-                    The output of the neural network. If an input of shape (m, n)
-                    is provided, an output of shape (M, n) is given where M is
-                    the number of nodes in the output layer."""
+            out : ndarray
+                The output of the neural network. If an input of shape (m, n)
+                is provided, an output of shape (M, n) is given where M is
+                the number of nodes in the output layer."""
 
-            for w, b, f in zip(self.weights, self.biases, self.functions):
-                a = f(w@a + b)
-            return a
+        for w, b, f in zip(self.weights, self.biases, self.functions):
+            a = f(w@a + b)
+        return a
 
-        def SGD(self, training_data, epochs, mini_batch_size, eta):
-            """Docstring to be updated.
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
+        """Docstring to be updated.
 
-            Parameters:
+        Parameters:
 
-                training_data : list of tuples of ndarrays
-                    List on the form [(x1, y1), (x2, y2), ...] where e.g.
-                    x1, y1 are column vectors of an input and the corresponding desired
-                    output of the neural network respectively.
+            training_data : list of tuples of ndarrays
+                List on the form [(x1, y1), (x2, y2), ...] where e.g.
+                x1, y1 are column vectors of an input and the corresponding desired
+                output of the neural network respectively.
 
-                epochs : int
-                    The number of epochs.
+            epochs : int
+                The number of epochs.
 
-                mini_batch_size : int
-                    The size of each mini_batch.
+            mini_batch_size : int
+                The size of each mini_batch.
 
-                eta : int or float
-                    The learning rate when applying gradient descent."""
+            eta : int or float
+                The learning rate when applying gradient descent."""
 
-            n = len(training_data)
-            for j in range(epochs):
-                np.random.shuffle(training_data)
-                mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
-                for k, mini_batch in enumerate(mini_batches):
-                    self.GD(mini_batch, eta)
-                print(f'epoch {j+1}/{epochs} complete')
+        n = len(training_data)
+        for j in range(epochs):
+            np.random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for k, mini_batch in enumerate(mini_batches):
+                self.GD(mini_batch, eta)
+            print(f'epoch {j+1}/{epochs} complete')
 
-        def GD(self, mini_batch, eta):
-            """Docstring to be updated."""
-            mini_batch_size = len(mini_batch)
-            nabla_w = [np.zeros(w.shape) for w in self.weights]
-            nabla_b = [np.zeros(b.shape) for b in self.biases]
-            for x, y in mini_batch:
-                delta_nabla_w, delta_nabla_b = self.backprop(x, y)
-                nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-                nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            self.weights = [w - eta*nw/mini_batch_size for w, nw in zip(self.weights, nabla_w)]
-            self.bias    = [b - eta*nb/mini_batch_size for b, nb in zip(self.biases, nabla_b)]
+    def GD(self, mini_batch, eta):
+        """Docstring to be updated."""
+        mini_batch_size = len(mini_batch)
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        for x, y in mini_batch:
+            delta_nabla_w, delta_nabla_b = self.backprop(x, y)
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+        self.weights = [w - eta*nw/mini_batch_size for w, nw in zip(self.weights, nabla_w)]
+        self.bias    = [b - eta*nb/mini_batch_size for b, nb in zip(self.biases, nabla_b)]
 
-        def backprop(self, x, y):
-            """Docstring to be updated."""
+    def backprop(self, x, y):
+        """Docstring to be updated."""
 
-            nabla_w = [np.zeros(w.shape) for w in self.weights]
-            nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
 
-            a = [x]
-            z = []
-            for w, b, f in zip(self.weights, self.biases, self.functions):
-                z.append(w@a[-1] + b)
-                a.append(f(z[-1]))
+        a = [x]
+        z = []
+        for w, b, f in zip(self.weights, self.biases, self.functions):
+            z.append(w@a[-1] + b)
+            a.append(f(z[-1]))
 
-            delta = self.cost_derivative(a[-1], y)*self.functions_derivative[-1](z[-1])
+        delta = self.cost_derivative(a[-1], y)*self.functions_derivative[-1](z[-1])
 
-            for l in range(self.num_layers, 0, -1):
-                nabla_w[l] = delta@a[l-1].T
-                nabla_b[l] = delta
-                delta = self.weights[l].T@delta * functions_derivative[l-1](z[l-1])
-            return nabla_w, nabla_b
+        for l in range(self.num_layers, 0, -1):
+            nabla_w[l] = delta@a[l-1].T
+            nabla_b[l] = delta
+            delta = self.weights[l].T@delta * functions_derivative[l-1](z[l-1])
+        return nabla_w, nabla_b
