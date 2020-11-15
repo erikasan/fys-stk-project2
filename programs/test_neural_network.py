cb1cdc20b4b97559707c072809eb3a4ad1e386af
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-11-15 14:54:10 +0100

    Update

--- programs/test_neural_network.py
+++ programs/test_neural_network.py
@@ -1,18 +1,10 @@
-import numpy as np
 from neural_network import NeuralNetwork
+import numpy as np
 
+x = np.array([1, 2, 3])
+x.shape = (x.shape[0], 1)
+y = 2*x
+net = NeuralNetwork([3, 3, 4])
 
-n_nodes = [1, 1]
-NN = NeuralNetwork(n_nodes = n_nodes, output_func = lambda x: x)
-NN.set_weights()
-NN.set_bias()
-
-def f(x):
-    return np.pi + np.exp(1)*x + 42*x**2
-
-x = [0]
-y = [f(0)]
-
-weights_gradient, bias_gradient = NN.back_propagation(x, y)
-
-NN.gradient_descent(weights_gradient, bias_gradient, iterations = 100000)
+training_data = [(x, y)]
+net.SGD(training_data, epochs=1, mini_batch_size=1, eta=1)
