9e6e991878d57b6c786e8a9b30aea4e5db43591b
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-11-18 13:49:35 +0100

    Update

--- programs/regression_with_neural_network.py
+++ programs/regression_with_neural_network.py
@@ -10,30 +10,32 @@ def FrankeFunction(x, y):
     term4 = - 0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
-x = np.random.random(10000)
-y = np.random.random(10000)
+x = np.random.random(1000)
+y = np.random.random(1000)
 z = FrankeFunction(x, y)
 
-input_data = [np.array([i, j]) for i, j in zip(x, y)]
-for arr in input_data:
-    arr.shape = (arr.shape[0], 1)
+def prepare_training_data(x, y, z):
+    assert len(x) == len(y) == len(z), "shape of x, y, z do not match"
+    input_data = [np.array([i, j]) for i, j in zip(x, y)]
+    for arr in input_data:
+        arr.shape = (arr.shape[0], 1)
 
-output_data = [np.array([k]) for k in z]
-for arr in output_data:
-    arr.shape = (arr.shape[0], 1)
+    output_data = [np.array([k]) for k in z]
+    for arr in output_data:
+        arr.shape = (arr.shape[0], 1)
 
-training_data = [(i, o) for i, o in zip(input_data, output_data)]
+    training_data = [(i, o) for i, o in zip(input_data, output_data)]
+    return training_data
 
 
-layers = [2, 30, 1]
+training_data = prepare_training_data(x, y, z)
+
+layers = [2, 20, 1]
 net = NeuralNetwork(layers=layers, mode='regression')
 net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3, lmbda=0)
 
-# input = np.array([0,0])
-# input.shape = (input.shape[0], 1)
 ztilde  = net.feedforward(input_data)
 ztilde  = ztilde.flatten()
 z_exact = z.flatten()
-
 
 print(f"MSE = {MSE(z_exact, ztilde)}")
