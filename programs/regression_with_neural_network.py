51e66fcd2d89d6309110fca253e8c5c2493732db
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-11-18 14:04:22 +0100

    Split data into train and test

--- programs/regression_with_neural_network.py
+++ programs/regression_with_neural_network.py
@@ -10,7 +10,7 @@ def FrankeFunction(x, y):
     term4 = - 0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
-def prepare_training_data(x, y, z):
+def prepare_data(x, y, z):
     assert len(x) == len(y) == len(z), "shape of x, y, z do not match"
     input_data = [np.array([i, j]) for i, j in zip(x, y)]
     for arr in input_data:
@@ -28,15 +28,20 @@ x = np.random.random(1000)
 y = np.random.random(1000)
 z = FrankeFunction(x, y)
 
-input_data, output_data, training_data = prepare_training_data(x, y, z)
+x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x, y, z, train_size=0.8)
 
-layers = [2, 20, 1]
+input_train, output_train, training_data = prepare_data(x_train, y_train, z_train)
+input_test, output_test, testing_data = prepare_data(x_test, y_test, z_test)
+
+layers = [2, 20, 1] # Input layer must be 2 nodes, output layer must be 1 node, since Franke is F : R^2 -> R^1
+                    # Anything inbetween is arbitrary
+
 net = NeuralNetwork(layers=layers, mode='regression')
 net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3, lmbda=0)
 
 
-ztilde  = net.feedforward(input_data)
+ztilde  = net.feedforward(input_test)
 ztilde  = ztilde.flatten()
-z_exact = z.flatten()
+z_exact = z_test.flatten()
 
 print(f"MSE = {MSE(z_exact, ztilde)}")
